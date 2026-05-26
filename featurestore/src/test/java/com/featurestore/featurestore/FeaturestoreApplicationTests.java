7f10c15fd98fe617ff0bbcbb9515095aa9d537b2
renanjfa <renanjusan@gmail.com>
2026-05-26 20:55:11 -0300

    SpringBoot Initializer featurestore

---
+++ featurestore/src/test/java/com/featurestore/featurestore/FeaturestoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.featurestore.featurestore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeaturestoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
