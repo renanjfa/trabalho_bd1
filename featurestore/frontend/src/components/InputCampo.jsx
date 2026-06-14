fd3abe9a456c710c2c269e67aef6668cce1e8b7b
Juan Jose <jjtorreshernandez@gmail.com>
2026-06-14 19:24:27 -0300

    correcao no compoente de input

--- featurestore/frontend/src/components/InputCampo.jsx
+++ featurestore/frontend/src/components/InputCampo.jsx
@@ -1,8 +1,8 @@
-export default function InputCampo({icon: Icon, type, placeholder}){
+export default function InputCampo({icon: Icon, type, placeholder, value, onChange}){
     return(
         <div className="flex items-center w-full h-11 border border-gray-400 rounded-md bg-white px-4">
             <Icon size={19} className="text-black mr-4"/>
-            <input type={type} placeholder={placeholder} className="w-full outline-none bg-transparent text-sm text-gray-700 placeholder:text-gray-400"/>
+            <input type={type} placeholder={placeholder} value={value} onChange={(e)=> onChange(e.target.value)} className="w-full outline-none bg-transparent text-sm text-gray-700 placeholder:text-gray-400"/>
         </div>
     );
 }
