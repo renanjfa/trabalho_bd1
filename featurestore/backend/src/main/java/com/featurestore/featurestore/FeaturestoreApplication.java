06f81b7f51144e07dc7a8f3bb1ab1a572d3e4a38
renanjfa <renanjusan@gmail.com>
2026-06-15 00:41:02 -0300

    correcao datasource mvnw run

--- featurestore/backend/src/main/java/com/featurestore/featurestore/FeaturestoreApplication.java
+++ featurestore/backend/src/main/java/com/featurestore/featurestore/FeaturestoreApplication.java
@@ -2,8 +2,9 @@ package com.featurestore.featurestore;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class FeaturestoreApplication {
 
 	public static void main(String[] args) {
