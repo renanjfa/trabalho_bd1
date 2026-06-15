f4544ac1c145c1de9c79828d13dcb36159f15156
renanjfa <renanjusan@gmail.com>
2026-06-15 20:20:58 -0300

    Erro import em DataSourceAutoConfiguration

--- featurestore/backend/src/main/java/com/featurestore/featurestore/FeaturestoreApplication.java
+++ featurestore/backend/src/main/java/com/featurestore/featurestore/FeaturestoreApplication.java
@@ -2,7 +2,7 @@ package com.featurestore.featurestore;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class FeaturestoreApplication {
