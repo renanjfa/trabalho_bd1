45f2807f7bc0271642218cd49091da30e779e5d1
renanjfa <renanjusan@gmail.com>
2026-07-04 16:43:50 -0300

    Implementacao DTOs

--- featurestore/backend/src/main/java/com/featurestore/featurestore/dto/QntAcessoDownloadDatasetDTO.java
+++ featurestore/backend/src/main/java/com/featurestore/featurestore/dto/QntAcessoDownloadDatasetDTO.java
@@ -1,5 +1,24 @@
 package com.featurestore.featurestore.dto;
 
 public class QntAcessoDownloadDatasetDTO {
-    
+
+    private Integer qnt_acessos;
+    private Integer qnt_downloads;
+
+    public Integer getQntAcessos() {
+        return this.qnt_acessos;
+    }
+
+    public void setQntAcessos(Integer qnt) {
+        this.qnt_acessos = qnt;
+    }
+
+    public Integer getQntDownloads() {
+        return this.qnt_downloads;
+    }
+
+    public void setQntDownloads(Integer qnt) {
+        this.qnt_downloads = qnt;
+    }
+
 }
--- featurestore/backend/src/main/java/com/featurestore/featurestore/dto/SistemaDTO.java
+++ featurestore/backend/src/main/java/com/featurestore/featurestore/dto/SistemaDTO.java
@@ -2,4 +2,32 @@ package com.featurestore.featurestore.dto;
 
 public class SistemaDTO {
     
+    private Integer qnt_datasets;
+    private Integer qnt_versoes;
+    private Integer qnt_usuarios;
+
+    public Integer getQntDatasets() {
+        return this.qnt_datasets;
+    }
+
+    public void setQntDatasets(Integer qnt) {
+        this.qnt_datasets = qnt;
+    }
+
+    public Integer getQntVersoes() {
+        return this.qnt_versoes;
+    }
+
+    public void setQntVersoes(Integer qnt) {
+        this.qnt_versoes = qnt;
+    }
+
+    public Integer getQntUsuarios() {
+        return this.qnt_usuarios;
+    }
+
+    public void setQntUsuarios(Integer qnt) {
+        this.qnt_usuarios = qnt;
+    }
+
 }
